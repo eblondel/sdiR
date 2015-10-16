c49ea21cfe55ccfc9135909d8277b367fac9c0f4
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2015-10-16 18:01:42 +0200

    provide computation for all fishing area levels

--- R/process_spatialjoin.R
+++ R/process_spatialjoin.R
@@ -18,10 +18,12 @@ fareas <- readWFS("http://www.fao.org/figis/geoserver/ows?service=WFS&version=1.
 fareaLevels <- unique(fareas@data$F_LEVEL)
 print(fareaLevels)
 
-#proceed to the spatial join	
-output <- joinByArea(spdf, fareas, "F_CODE", filter = list(key = "F_LEVEL", value = fareaLevels[1]), na.rm = TRUE)
-result <- as.data.frame(output)
+#proceed to the spatial join and write outputs for all levels except F_SUBUNIT
+for(i in 1:(length(fareaLevels)-1)){
+	#proceed to the spatial join
+	output <- joinByArea(spdf, fareas, "F_CODE", filter = list(key = "F_LEVEL", value = fareaLevels[i]), na.rm = TRUE)
+	result <- as.data.frame(output)
 
-#write result
-write.csv(result, file = "output.csv", row.names = FALSE)
-
+	#write result
+	write.csv(result, file = paste0("output_", tolower(fareaLevels[i]), ".csv"), row.names = FALSE)
+}
