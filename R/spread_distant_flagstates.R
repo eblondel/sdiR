804dab10ddbff89ef5fc3d9507ee9f388520b982
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2016-09-26 16:13:43 +0200

    Update spread_distant_flagstates.R

--- R/spread_distant_flagstates.R
+++ R/spread_distant_flagstates.R
@@ -1,3 +1,4 @@
+
 #
 # Scripts to experiment SPREAD-like algorithm for catch statistics of
 # distant countries, based on % of catches by coastal countries, by 
@@ -47,34 +48,89 @@ roundingDecimals <- 0
 
 # @title computeWprobByDims
 # @description
-# unit function to compute by area/year the probability model which will give
+# unit function to compute by area/year/flagstate the probability model which will give
 # probability values by target area (represented by a combination of FAO area,
-# LME (reclassified) and EEZ (reclassified as ISO3 code)
+# LME (reclassified) and EEZ (reclassified as ISO3 code). This function will distinguish
+# among DISTANT vs. COASTAL flagstates
 # @param intersects an object of class "data.frame" giving the intersects
 # @param stats an object of class "data.frame" giving the catch statistics
 # @param area an object of class "integer" or "character" (FAO major area)
-# @param area an object of class "integer" or "character" (FAO major area)
+# @param year an object of class "integer" or "character" (year)
+# @param flagstate an object of class "character" (ISO3 code of flagstate)
+# @param includeDistantCoastalWaters an object of class "logical". Default is FALSE. If TRUE, then in case of EEZs
+# on which the distant flagstate has sovereignty, a probably will be assigned corresponding to the % of target intersect
+# surface on the total intersect surface in the area.
+# @param useCoastalWaterSurfaces object of class "logical". Default is FALSE. If TRUE, in case of coastal flagstates,
+# the surface will be used as probability to reallocate values among multiple waters.
 # @return an object of class "data.frame" giving the probability model to use for reallocation
-computeWprobByDims <- function(intersects, stats, area, year){
-	
+computeWprobByDims <- function(intersects, stats, area, year, flagstate,
+								includeDistantCoastalWaters = FALSE,
+								useCoastalWaterSurfaces = FALSE){
+
+	isDistant <- TRUE
+	out.wprobs <- NULL
+
 	#extract stats for coastal flagstates only
 	area.intersects <- intersects[intersects$F_AREA == area,]
-	flagstates <- unique(area.intersects[,"ISO_3digit"])
-	out.stats <- stats[stats$FIC_SYS_CATCH_AREA == area
-						& stats$YR_ITEM == year
-						& stats$ISO_3_CODE %in% flagstates,]
 	
-	#compute wprob
-	out.stats$wprob <- out.stats$SumOfQUANTITY / sum(out.stats$SumOfQUANTITY)
-	out.stats <- out.stats[,c("FIC_SYS_CATCH_AREA", "ISO_3_CODE", "YR_ITEM", "wprob")]
+	#determine if flagstate is considered as DISTANT or COASTAL in this area
+	fsIdx <- area.intersects$ISO_3digit == flagstate
+	fs <- area.intersects[fsIdx,]
+	if(nrow(fs) > 0){
+		isDistant <- !any(fs$Country == fs$Sovereign)
+	}
 	
-	#merge intersects with wprob
-	out.wprobs <- merge(x = intersects, y = out.stats,
+	if(isDistant){
+		#Case of distant flagstates
+		#--------------------------
+		coastal <- area.intersects
+		if(nrow(fs) > 0) coastal <- area.intersects[!fsIdx,]
+		flagstates <- unique(coastal[,"ISO_3digit"])
+		
+		#probabilities without considering potential EEZ on which the flagstate has sovereignty
+		out.stats <- stats[stats$FIC_SYS_CATCH_AREA == area
+					& stats$YR_ITEM == year
+					& stats$ISO_3_CODE %in% flagstates,]			
+		
+		if(nrow(out.stats) > 0){	
+			#compute wprob
+			out.stats$flagstate <- flagstate
+			out.stats$wprob <- out.stats$SumOfQUANTITY / sum(out.stats$SumOfQUANTITY)
+			out.stats <- out.stats[,c("FIC_SYS_CATCH_AREA", "ISO_3_CODE", "YR_ITEM", "flagstate", "wprob")]
+			
+			#merge intersects with wprob
+			out.wprobs <- merge(x = intersects, y = out.stats,
 						by.x = c("F_AREA", "ISO_3digit"), by.y = c("FIC_SYS_CATCH_AREA", "ISO_3_CODE"), all.y = TRUE)
-	out.wprobs <- out.wprobs[order(as.factor(out.wprobs$EcoTypeNew)),]
-	out.wprobs <- out.wprobs[!duplicated(out.wprobs[,c("F_AREA", "ISO_3digit", "wprob")], fromLast = TRUE),]
-	return(out.wprobs)
+			out.wprobs <- out.wprobs[order(as.factor(out.wprobs$EcoTypeNew)),]
+			out.wprobs <- out.wprobs[!duplicated(out.wprobs[,c("F_AREA", "ISO_3digit", "wprob")], fromLast = TRUE),]
+		}
+		
+		if(nrow(fs) > 0){
+			if(includeDistantCoastalWaters){
+				#compute the prob to associate to EEZ on which the distant flagstate has sovereignty
+				fsprob <- 1 - sum(out.wprobs$Surface) / sum(area.intersects$Surface)
+				out.wprobs <- rbind(
+					cbind(fs, YR_ITEM = year, flagstate = flagstate, wprob = fsprob),
+					out.wprobs
+				)
+			}
+		}
 	
+	}else{
+		#Case of coastal flagstates
+		#--------------------------
+		coastal.probs <- switch(as.character(useCoastalWaterSurfaces),
+			"TRUE" = fs$Surface / sum(fs$Surface),
+			"FALSE" = rep(1/nrow(fs), nrow(fs))
+		)
+		out.wprobs <- cbind(fs, YR_ITEM = year, flagstate = flagstate, wprob = coastal.probs)
+	}
+	
+	cnames <- c("F_AREA", "ISO_3digit", "EEZ", "Country", "Sovereign", "FID_LME_re", 
+				"LME_NUMBER", "LME_NAME", "Classified", "SSTNPPCODE", "EcoTypeNew", 
+				"SSTNPPCODN", "LonCentr", "ISO3_FSA", "Surface", "YR_ITEM", "flagstate", "wprob")
+	out.wprobs <- out.wprobs[, cnames]
+	return(out.wprobs)
 }
 
 # @title computeWprob
@@ -84,43 +140,38 @@ computeWprobByDims <- function(intersects, stats, area, year){
 # EEZ (reclassified as ISO3 code)
 # @param intersects an object of class "data.frame" giving the intersects
 # @param stats an object of class "data.frame" giving the catch statistics
+# @param includeDistantCoastalWaters an object of class "logical". Default is FALSE. If TRUE, then in case of EEZs
+# on which the distant flagstate has sovereignty, a probably will be assigned corresponding to the % of target intersect
+# surface on the total intersect surface in the area.
+# @param useCoastalWaterSurfaces object of class "logical". Default is FALSE. If TRUE, in case of coastal flagstates,
+# the surface will be used as probability to reallocate values among multiple waters.
+# @param verbose if logs have to be provided. Default is FALSE.
 # @return an object of class "data.frame" giving the probability model to use for reallocation
-computeWprob <- function(intersects, stats){	
-	dims <- unique(stats[,c("FIC_SYS_CATCH_AREA","YR_ITEM")])
-	wprobs <- do.call("rbind", lapply(1:nrow(dims),
+computeWprob <- function(intersects, stats,
+						 includeDistantCoastalWaters = FALSE,
+						 useCoastalWaterSurfaces = FALSE,
+						 verbose = FALSE){
+						 
+	dims <- unique(stats[,c("FIC_SYS_CATCH_AREA","YR_ITEM", "ISO_3_CODE")])
+	wprobs.list <- lapply(1:nrow(dims),
 						function(i){
+							trgArea <- dims[i,"FIC_SYS_CATCH_AREA"]
+							trgYear <- dims[i,"YR_ITEM"]
+							trgFlagstate <- dims[i, "ISO_3_CODE"]
+							cat(sprintf("Computing wprobs for Area %s / year %s / flagstate '%s' \n", trgArea, trgYear, trgFlagstate))
 							out <- computeWprobByDims(
 										intersects, stats,
-										area = dims[i,"FIC_SYS_CATCH_AREA"],
-										year = dims[i,"YR_ITEM"])
+										area = trgArea,
+										year = trgYear,
+										flagstate = trgFlagstate,
+										includeDistantCoastalWaters = includeDistantCoastalWaters,
+										useCoastalWaterSurfaces = useCoastalWaterSurfaces)
 							return(out)
-						}))
+						})
+	wprobs.list <- wprobs.list[!sapply(wprobs.list, is.null)]
+	wprobs <- do.call("rbind", wprobs.list)
 	return(wprobs)
 }
-
-# @title getForeignCatches
-# @description
-# function to get catches done by foreign flagstates in coastal areas
-# foreign flagstates are obtained by difference of coastal flagstates
-# (obtained by intersect between EEZ/FAO area)
-# @param intersects an object of class "data.frame" giving the intersects
-# @param stats an object of class "data.frame" giving the catch statistics
-# @return an object of class "data.frame" giving the foreign catch statistics
-getForeignCatches <- function(intersects, stats){	
-	out <- do.call("rbind",
-		lapply(unique(intersects$F_AREA),
-			function(area){
-				#extract catch statistics of distant flagstates
-				area.intersects <- intersects[intersects$F_AREA == area,]
-				flagstates <- unique(area.intersects[,"ISO_3digit"])
-				out.stats <- stats[stats$FIC_SYS_CATCH_AREA == area
-									& !(stats$ISO_3_CODE %in% flagstates)
-									& stats$SumOfQUANTITY > 0,]
-				return(out.stats)
-			}))		
-	return(out)
-}
-
 
 #execution & outputs
 #==========================================================================
@@ -132,27 +183,23 @@ cat(paste0("Started at: ", as.character(processStartingTime),"\n"))
 #-------
 cat("Building probability model for reallocation...\n")
 #compute probabilities
-wprobs <- computeWprob(intersects, stats)
+wprobs <- computeWprob(intersects, stats, includeDistantCoastalWaters = TRUE, useCoastalWaterSurfaces = TRUE)
 
 #validation of probabilities
-aggregate(wprobs$wprob, by = as.list(wprobs[c("F_AREA","YR_ITEM")]), FUN = "sum")
+#aggregate(wprobs$wprob, by = as.list(wprobs[c("F_AREA","YR_ITEM")]), FUN = "sum")
 
-#step 2: extract catches for distant countries
-#-------
-cat("Grabing foreign catches...\n")
-foreign_catches <- getForeignCatches(intersects, stats)
 
-#step 3: reallocate based on probability model computed in step 1
+#step 2: reallocate based on probability model computed in step 1
 #(the function "reallocate" is moved to 'spread' package)
 #-------
 cat("Reallocating statistical data...\n")
 result <- spread::reallocate(
-		x = foreign_catches,
+		x = stats,
 		y = wprobs,
 		area.x = "FIC_SYS_CATCH_AREA",
 		area.y = "F_AREA",
-		by.x = "YR_ITEM",
-		by.y = "YR_ITEM",
+		by.x = c("YR_ITEM", "ISO_3_CODE"),
+		by.y = c("YR_ITEM", "flagstate"),
 		data = "SumOfQUANTITY",
 		warea = NULL,
 		wprob = "wprob",
